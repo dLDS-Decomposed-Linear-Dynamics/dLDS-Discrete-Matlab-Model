18660f9101bad3aa7b98e11924f861656717a3cb
EYezerets <eyezerets@gmail.com>
2024-09-06 14:37:01 -0400

    Revert "Added subplots to look at behavior-related variables"
    This reverts commit ba830ba9cd81de6eef40de0f8e4f0837c112cea7.

--- code/plotMultiNetworkOutput.m
+++ code/plotMultiNetworkOutput.m
@@ -1,23 +1,17 @@
-function plotMultiNetworkOutput(tSel, dFF, A_cell, B_cell, groundTruthStates, F,varargin)
-if nargin > 6
-    nLines = 7;
-    Psi = varargin{1};
-    simulatedPsi = varargin{2};
-else
-    nLines = 5;
-end
+function plotMultiNetworkOutput(tSel, dFF, A_cell, B_cell, groundTruthStates, F)
+
 
 
 % tSel = 5; % selected trial
 figure(1), cla; 
-subplot(nLines,4,[1,4]), plot(dFF{tSel}.')       % ASC added 7/25
+subplot(5,4,[1,4]), plot(dFF{tSel}.')       % ASC added 7/25
 box off; xlabel('Time (frames)'); ylabel('Signal (GT)')
 set(gca,'XLim',[1,size(dFF{tSel},2)]);
-subplot(nLines,4,[5,8]), plot(A_cell{tSel}.')    % ASC added 7/25
+subplot(5,4,[5,8]), plot(A_cell{tSel}.')    % ASC added 7/25
 set(gca,'XLim',[1,size(dFF{tSel},2)]);
 box off; xlabel('Time (frames)'); ylabel('Reconstruction')
 
-subplot(nLines,4,[9,12]), imagesc(abs(B_cell{tSel}))   % ASC added 7/25
+subplot(5,4,[9,12]), imagesc(abs(B_cell{tSel}))   % ASC added 7/25
 set(gca,'XLim',[1,size(dFF{tSel},2)]);
 box off; xlabel('Time (frames)'); ylabel('Coeff Index')
 
@@ -25,7 +19,7 @@ box off; xlabel('Time (frames)'); ylabel('Coeff Index')
 % set(gca,'XLim',[1,size(dFF{tSel},2)]);
 % box off; xlabel('Time (frames)'); ylabel('Dynamics coefficient amplitude')
 % legend('C1','C2','C3','C4')
-subplot(nLines,4,[13,16]), imagesc(groundTruthStates{tSel})   % ASC added 7/25
+subplot(5,4,[13,16]), imagesc(groundTruthStates{tSel})   % ASC added 7/25
 box off; xlabel('Time (frames)'); ylabel('True state'); ylim('padded')
 %set(gca,'XLim',[1,size(dFF{tSel},2)],'YLim',[-0.1,1.1]);
 % legend('C1','C2','C3','C4')
@@ -35,14 +29,8 @@ nF1   = size(F{1},1);
 for ll = 2:numel(F)
     allFs = cat(2,allFs,[zeros(nF1,5),F{ll}]);
 end
-subplot(nLines,4,[17,20]), imagesc(allFs); 
+subplot(5,4,[17,20]), imagesc(allFs); 
 axis image; axis off; colormap gray
-
-if nargin > 6
-    subplot(nLines,4,[21,24]), imagesc(Psi); 
-    subplot(nLines,4,[25,28]), imagesc(simulatedPsi); 
-end
-
 % subplot(5,4,17), imagesc(F{1}); axis image; axis off; colormap gray
 % subplot(5,4,18), imagesc(F{2}); axis image; axis off; colormap gray
 % subplot(5,4,19), imagesc(F{3}); axis image; axis off; colormap gray
