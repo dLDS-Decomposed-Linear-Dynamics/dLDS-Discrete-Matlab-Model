b4a80b64c18b104928cd6b89d7fa6b5db05045d2
EYezerets <eyezerets@gmail.com>
2024-09-06 14:36:05 -0400

    Revert "behavior code and new parameters"
    This reverts commit d0a804c9b2eefeabac02f43a4bdf6ce57efca688.

--- code/Dynamics_Learning/subfunctions/sample_dynamic_exemplars.m
+++ code/Dynamics_Learning/subfunctions/sample_dynamic_exemplars.m
@@ -19,9 +19,7 @@ x_im  = cell(1,opts.N_ex);                                                 % Ini
 %% Get Training Data. 
 
 if isnumeric(data_obj)&&(numel(size(data_obj))==2)
-
     data_use_ind = ceil((size(data_obj, 2)-Ts+1)*rand(1, opts.N_ex));      % Select a sampling of the data blindly
-    
     for kk = 1:opts.N_ex
         x_im{kk} = data_obj(:,data_use_ind(kk):(data_use_ind(kk)+Ts-1));     % Extract a portion of the sequence
         if (opts.ssim_flag)&&(sqrt(sum(x_im{kk}(:).^2))>0)            % If desired, normalize the sequence
@@ -44,36 +42,16 @@ elseif isnumeric(data_obj)&&(numel(size(data_obj))==3)                     % Can
     end
 elseif iscell(data_obj)&&(numel(size(data_obj{1}))==2)
     num_im    = length(data_obj);                                          % Get sizes of the data blocks (color images or video):
-    samplesCounter = 0;
-    timePointsSum = 0;
-    for jj = 1:num_im
-        timePointsSum = timePointsSum + size(data_obj{jj},2);
-    end
-    for jj = 1:num_im
-        if opts.sampleProportionally
-            trialProportion = size(data_obj{jj},2)/timePointsSum;
-        else
-            trialProportion = 1/num_im;
+    height_im = size(data_obj{1}, 2) - Ts + 1;                             % Get heights and widths for allowable starting indices for subimages
+    start_ind = ceil(bsxfun(@times,rand(opts.N_ex,2),[height_im, num_im]));  % Pick random starting points
+    for kk = 1:opts.N_ex
+        x_im{kk} = data_obj{start_ind(kk,2)}(:,...
+                            start_ind(kk,1):start_ind(kk,1)+Ts-1); % Pick out subimage and reshape it to a vector
+        if (opts.ssim_flag)&&(sqrt(sum(x_im{kk}(:).^2))>0)            % If desired, normalize the sequence
+                x_im{kk} = x_im{kk}./sqrt(sum(x_im{kk}(:).^2));            % Normalize the chosen sample, as long as the norm is not zero
         end
-        if jj == num_im
-            nSamplesThisTrial = opts.N_ex - samplesCounter;
-        else
-            nSamplesThisTrial = ceil(trialProportion*opts.N_ex);
-        end
-        
-        height_im = size(data_obj{jj}, 2) - Ts + 1;                             % Get heights and widths for allowable starting indices for subimages
-        start_ind = ceil(bsxfun(@times,rand(nSamplesThisTrial,1),height_im));  % Pick random starting points
-        for kk =  1:nSamplesThisTrial %opts.N_ex
-            x_im{samplesCounter +kk} = data_obj{jj}(:,...
-                                start_ind(kk):start_ind(kk)+Ts-1); % Pick out subimage and reshape it to a vector
-            if (opts.ssim_flag)&&(sqrt(sum(x_im{samplesCounter +kk}(:).^2))>0)            % If desired, normalize the sequence
-                    x_im{samplesCounter +kk} = x_im{samplesCounter +kk}./sqrt(sum(x_im{samplesCounter +kk}(:).^2));            % Normalize the chosen sample, as long as the norm is not zero
-            end
-        end
-        samplesCounter = samplesCounter + nSamplesThisTrial;
     end
 elseif iscell(data_obj)&&(numel(size(data_obj{1}))==3)
-    disp('Note: sample_dynamic_examplars still uses dimensions of the first object in cell array for 3d images - must fix if you use this functionality')
     num_im = length(data_obj);                                             % Get sizes of the data blocks (color images or video):
     height_im = size(data_obj{1}, 1) - opts.bl_size(1) + 1;                % Get heights and widths for allowable starting indices for subimages
     width_im = size(data_obj{1}, 2) - opts.bl_size(2) + 1;
